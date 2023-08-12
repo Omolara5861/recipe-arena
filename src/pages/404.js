cd5d4b49b1b62e0c951c4d501385fa865f8d5982
Omolara5861 <debbiegterra@gmail.com>
2023-08-12 01:05:24 +0100

    chore: :tada: Create pages for project

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+const Error = () => {
+    return (
+        <Layout>
+            <h1>Error</h1>
+        </Layout>
+    )
+}
+
+export default Error;
---
+++ src/pages/recipes.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+const Recipes = () => {
+    return (
+    <Layout>
+            <h1>Recipes</h1>
+    </Layout>
+    )
+}
+
+export default Recipes;
