77455a2ac495499f6df4729145cbf1a9d426d657
Omolara5861 <debbiegterra@gmail.com>
2023-08-12 01:15:28 +0100

    feat: :sparkles: Ceate nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
-const Tags = () => {
+import { Link } from 'gatsby';
+const Header = () => {
     return (
         <Layout>
-            <h1>Tags</h1>
+            <nav>
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                    <li><Link to='contact'>Contact</Link></li>
+                    <li><Link to='/recipes'>Recipes</Link></li>
+                    <li><Link to='/tags'>Tags</Link></li>
+                </ul>
+            </nav>
         </Layout>
     )
 }
 
-export default Tags;
+export default Header;
