cb3f43639e8507cf4055888c2cd5fbe4af681a43
Omolara5861 <debbiegterra@gmail.com>
2023-10-07 20:40:57 +0100

    feat: :sparkles: Add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react"
+
 const Footer = () => {
-    return (
-        <p>Footer</p>
-    )
+  return (
+    <footer className="page-footer">
+      <p>
+        &copy; {new Date().getFullYear()} <span>Recipes Arena</span>. Built with{" "}
+        <a href="https://www.gatsbyjs.com/">Gatsby</a>
+      </p>
+    </footer>
+  )
 }
 
-export default Footer;
+export default Footer
