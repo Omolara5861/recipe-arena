ea75a0862813034ab5801fd65746e1d14b613163
Omolara5861 <debbiegterra@gmail.com>
2023-10-08 21:41:46 +0100

    chore: :fire: Remove unused import

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
-import { Link, graphql } from "gatsby"
+import { Link} from "gatsby"
 
 const About = () => {
   return (
