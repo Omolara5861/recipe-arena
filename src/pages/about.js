884be76cfd644994d5bf4ea01650c3d77ea173c5
Omolara5861 <debbiegterra@gmail.com>
2023-10-09 23:23:54 +0100

    refactor: :recycle: Refactor code

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,14 +10,14 @@ const About = () => {
         <section className="about-page">
           <article>
             <h2>I'm baby coloring book poke taxidermy</h2>
-            // <p>
-            //   Taxidermy forage glossier letterpress heirloom before they sold
-            //   out you probably haven't heard of them banh mi biodiesel chia.
-            // </p>
-            // <p>
-            //   Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvia
-            //   retro.
-            // </p>
+            <p>
+              Taxidermy forage glossier letterpress heirloom before they sold
+              out you probably haven't heard of them banh mi biodiesel chia.
+            </p>
+            <p>
+              Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvia
+              retro.
+            </p>
             <Link to="/contact" className="btn">
               contact
             </Link>
