3944acadd5de247eeb24865873219cb1c2b95a33
Omolara5861 <debbiegterra@gmail.com>
2023-10-10 16:46:47 +0100

    feat: :sparkles: Create header for homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,26 @@
-import * as React from "react"
+import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 import Layout from '../components/Layout';
 export default function Home() {
   return (
     <Layout>
-      <h1>Homepage</h1>
+       <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="eggs"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          ></StaticImage>
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>simply recipes</h1>
+              <h4>no fluff, just recipes</h4>
+            </div>
+          </div>
+        </header>
+      </main>
   </Layout>
   )
 }
